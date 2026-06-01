230a5b0d03c52f1312648dc4793bbe1ea4bdd5b1
Michael Hughes <microneesia79@gmail.com>
2026-05-31 17:45:35 -0700

    Register local-first service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { registerServiceWorker } from './registerServiceWorker';
 import './styles.css';
 import './styles-v04.css';
 
@@ -9,3 +10,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
   </React.StrictMode>,
 );
+
+registerServiceWorker();
