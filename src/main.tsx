bebd29dbc3b7f3e7fe60619bea7b3a8b1b602ff0
Michael Hughes <microneesia79@gmail.com>
2026-05-31 19:55:08 -0700

    Import v1.5 Consent Language styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './styles-v04.css';
 import './styles-v12.css';
 import './styles-v13.css';
 import './styles-v14.css';
+import './styles-v15.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
